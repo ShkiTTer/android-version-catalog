b9bf35e8ac112abfad7b8b6bf0dd026e0b03864e
Maxim Sachuk <m.sachuk@redmadrobot.com>
2022-09-09 11:19:50 +0700

    Add publication script

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     `version-catalog`
 }
 
+repositories {
+    mavenCentral()
+}
+
 allprojects {
     group = "com.github.redmadrobot-tomsk.gradle-version-catalogs"
     version = "2022.09.08"
@@ -14,9 +18,29 @@ subprojects {
         plugin("org.gradle.maven-publish")
     }
 
+    repositories {
+        mavenCentral()
+    }
+
     catalog {
         versionCatalog {
             from(files("libs.versions.toml"))
+        }
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["versionCatalog"])
+
+                afterEvaluate {
+                    pom {
+                        name.set(project.name)
+                        description.set(project.description)
+                        version = project.version.toString()
+                    }
+                }
+            }
         }
     }
 }
